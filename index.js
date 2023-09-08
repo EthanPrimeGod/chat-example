797e777c5ebfc9327be863918f963dcbef351364
EthanPrimeGod <144484272+EthanPrimeGod@users.noreply.github.com>
2023-09-08 14:43:57 -0700

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 const path = require('path')
 
